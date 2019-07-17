ab01ca3dea56db0262a8ecdc4bc7c39a3a3626d5
Alyson Michel <52330186+ahmichel93@users.noreply.github.com>
2019-07-16 22:33:14 -0300

    aplicações com consign

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
+var connMySQL = function(){
+  //console.log('Conexao com o bd da aplicacao ok');
+  return connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'portal_noticias'
+  });
+}
+
 module.exports = function(){
-    return connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'portal_noticias'
-    });
-};
+    //console.log('conexao com o bd ok');
+    return connMySQL;
+}
