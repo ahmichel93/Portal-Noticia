e490f107ccd66d9c6c3af2a2f9e76b535217d174
Alyson Michel <52330186+ahmichel93@users.noreply.github.com>
2019-07-23 23:12:07 -0300

    Express Validation
    Correção de bug's, validação de campos obrigatórios no formulário.

--- app/router/admin.js
+++ app/router/admin.js
@@ -7,6 +7,21 @@ module.exports = function(app){
   app.post('/noticias/salvar', function(req, res){
     var noticia = req.body;
 
+    //console.log(noticia);
+    req.assert('titulo','Título é obrigatorio').notEmpty();
+    req.assert('resumo','Resumo é obrigatorio').notEmpty();
+    req.assert('resumo','Resumo deve conter entre 05 e 100 caracteres').len(5, 100);
+    req.assert('Autor','Autor é obrigatorio').notEmpty();
+    req.assert('data_noticia','Data é obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+    req.assert('noticia','Noticia é obrigatorio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+      res.render("admin/form_add_noticia");
+      return;
+    }
+
     var connection = app.config.dbConnection();
     var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
